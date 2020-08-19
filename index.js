53c6a02dc65916ad160fbba2b111bb03c91c3648
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-08-19 14:49:26 -0700

    lab-08 initial commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const mongoose = require('mongoose');
 const server = require('./lib/server.js');
 
+
+//need to change to make my own
+// const MONGODB_URI ='mongo://localhost:27017/products';
+
+// const mongooseOptions = {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// };
+
+// mongoose.connect(MONGODB_URI, mongooseOptions);
 server.start(3000);
--- middleware/404.js
+++ middleware/404.js
@@ -1,11 +1,5 @@
 'use strict';
 
-/*
-
-Sends a 404/Not-Found message as the response (does not call .next())
-Import this into your server and set it up to be “used” after your other routes
-
-*/
 
 module.exports = (request,response,next) => {
   console.log('Unknown Route');
